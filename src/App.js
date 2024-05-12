c2828e78e13725bf016ac80ff73a78f6e8f2167c
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-12 13:37:41 +0200

    created html, left with styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <img src='https://c8.alamy.com/comp/P84DGT/cartoon-of-man-with-artificial-smile-taking-selfie-P84DGT.jpg' alt='My selfie' className='selfieImg' />
+      </div>
+      <div>
+        <h1>Buhle Kasa</h1>
+        <h2>Front-End Web Developer</h2>
+      </div>
+      <div>
+        <a href='mailto:BuhleKasa@gmail.com'>BuhleKasa@gmail.com</a>
+      </div>
+      <div>
+        <button className='buttons'>✉️ Email</button>
+        <button className='buttons'><i className="fa fa-linkedin-square"></i> LinkedIn</button>
+      </div>
+      <div>
+        <h2 className="InfoHeadings">About</h2>
+        <p>I am a Front-End Developer with an interest in working with APIs and many other JavaScript and React features.</p>
+      </div>
+      <div>
+        <h2 className="InfoHeadings">Interests</h2>
+        <p>I am a Front-End Developer with an interest in working with APIs and many other JavaScript and React features.</p>
+      </div>
+      <footer className='footer'>
+        <a href="#" className="footerLinks fa fa-facebook-f"></a>
+        <a href="#" className="footerLinks fa fa-github"></a>
+        <a href="#" className="footerLinks fa fa-instagram"></a>
+      </footer>
     </div>
   );
 }
